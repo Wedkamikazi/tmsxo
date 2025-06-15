81cfe903f96d06a74c06cc0f1e6eb8ebc528d035
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 09:20:58 +0300

    Auto-commit: 1 modified file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { DataHub } from './components/DataHub';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { SystemInitializer } from './components/SystemInitializer';
 import './styles/globals.css';
 
 function App(): React.ReactElement {
   return (
     <ErrorBoundary componentName="App">
-      <div className="App">
-        <ErrorBoundary componentName="DataHub">
-          <DataHub />
-        </ErrorBoundary>
-      </div>
+      <SystemInitializer>
+        <div className="App">
+          <ErrorBoundary componentName="DataHub">
+            <DataHub />
+          </ErrorBoundary>
+        </div>
+      </SystemInitializer>
     </ErrorBoundary>
   );
 }

75321f0427322b1fb5718282094666a554ae8696
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 11:01:36 +0300

    Auto-commit: 1 modified file

--- src/services/systemIntegrityService.ts
+++ src/services/systemIntegrityService.ts
@@ -1,6 +1,6 @@
 import { eventBus, DataEvent } from './eventBus';
 import { localStorageManager } from './localStorageManager';
-import { unifiedDataService, StoredTransaction } from './unifiedDataService';
+import { unifiedDataService } from './unifiedDataService';
 import { performanceManager } from './performanceManager';
 import { crossTabSyncService } from './crossTabSyncService';
 import { Transaction, BankAccount, UploadedFile } from '../types';

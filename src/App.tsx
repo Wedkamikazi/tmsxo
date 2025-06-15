bbd09d50d098655f2d7b1eee524ac23d288698f5
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 00:35:27 +0300

    Auto-commit: 1 modified file

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ function App(): React.ReactElement {
                 `}</style>
               </div>
             }>
-              <DataHub />
+              <LazyDataHub />
             </React.Suspense>
           </ErrorBoundary>
         </div>

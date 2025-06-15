c5e22edf5738de1c51263d37a34c646b431b846c
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 12:29:03 +0300

    Auto-commit: src/components/BalanceValidationDialog.tsx at 6/15/2025, 12:29:03 PM

--- src/components/BalanceValidationDialog.tsx
+++ src/components/BalanceValidationDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BalanceValidationResult } from '../services/balanceManagementService';
+import { BalanceValidationResult } from '../services/unifiedBalanceService';
 import './BalanceValidationDialog.css';
 
 interface BalanceValidationDialogProps {

73959fee56d3621d0148a7d0b2f33b3cd77d4ae3
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 23:07:01 +0300

    Auto-commit: src/services/enhancedMLOrchestrator.ts at 6/15/2025, 11:07:01 PM

--- src/services/enhancedMLOrchestrator.ts
+++ src/services/enhancedMLOrchestrator.ts
@@ -3,6 +3,7 @@ import { Transaction, MLCategorizationResult } from '../types';
 import { localOllamaIntegration } from './localOllamaIntegration';
 // import { systemIntegrityService } from './systemIntegrityService';
 // import { performanceManager } from './performanceManager';
+import { cleanupManager } from './cleanupManager';
 import { localStorageManager } from './localStorageManager';
 import { categorizationService } from './categorizationService';
 import { isDebugMode } from '../utils/debugMode';

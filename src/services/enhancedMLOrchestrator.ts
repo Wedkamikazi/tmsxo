1be73849fe0c6c6f278f13fed725b769188a35d6
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 13:36:25 +0300

    Auto-commit: 1 modified file

--- src/services/enhancedMLOrchestrator.ts
+++ src/services/enhancedMLOrchestrator.ts
@@ -1,7 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 import { Transaction, MLCategorizationResult } from '../types';
 import { localOllamaIntegration } from './localOllamaIntegration';
-import { systemIntegrityService } from './systemIntegrityService';
+// import { systemIntegrityService } from './systemIntegrityService';
 import { performanceManager } from './performanceManager';
 import { localStorageManager } from './localStorageManager';
 import { categorizationService } from './categorizationService';

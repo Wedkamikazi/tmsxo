274fe5e81cf0f8721f559781e990d279fde9fb82
Wedkamikazi <wedkamikazi@github.com>
2025-06-15 18:35:41 +0300

    Auto-commit: 1 modified file

--- src/components/OllamaControlWidget.tsx
+++ src/components/OllamaControlWidget.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { systemSafetyManager } from '../utils/systemSafetyManager';
+import { checkOllamaStatus, startOllamaProcess, stopOllamaProcess } from '../utils/processController';
 import './OllamaControlWidget.css';
 
 interface OllamaStatus {

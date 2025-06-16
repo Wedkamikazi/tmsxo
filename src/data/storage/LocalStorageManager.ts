42f731534917e454b57a52fd0e3d6e906f42c8e4
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 19:44:38 +0300

    Auto-commit: src/data/storage/LocalStorageManager.ts at 6/16/2025, 7:44:38 PM

--- src/data/storage/LocalStorageManager.ts
+++ src/data/storage/LocalStorageManager.ts
@@ -1,8 +1,8 @@
 import { Transaction, BankAccount, UploadedFile, TransactionCategory, TransactionCategorization } from '../../../shared/types';
-import { performanceManager } from './performanceManager';
+import { performanceManager } from '../../core/performance/PerformanceManager';
 import { systemIntegrityService } from '../integrity/SystemIntegrityService';
-import { eventBus } from './EventBus';
-import { storageQuotaManager } from './storageQuotaManager';
+import { eventBus } from '../../core/orchestration/EventBus';
+import { storageQuotaManager } from './StorageQuotaManager';
 
 export interface StorageSnapshot {
   timestamp: string;

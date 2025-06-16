78066b25b75c765b28ec3580342976a0cf74a820
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 20:24:24 +0300

    Auto-commit: 1 modified file

--- src/data/maintenance/CleanupManager.ts
+++ src/data/maintenance/CleanupManager.ts
@@ -4,7 +4,7 @@
  * Prevents memory leaks and ensures proper resource cleanup
  */
 
-import { performanceManager } from './performanceManager';
+import { performanceManager } from '../../core/performance/PerformanceManager';
 import { systemSafetyManager } from '../core/safety/SystemSafetyManager';
 
 interface DisposableResource {

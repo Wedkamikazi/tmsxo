d331a0fd7d2bb0fe0a8aeba90ccee89068b05625
Wedkamikazi <wedkamikazi@github.com>
2025-06-17 00:57:21 +0300

    Auto-commit: 1 added file

---
+++ src/analytics/index.ts
@@ -0,0 +1,11 @@
+// Analytics module exports
+export { unifiedCategorizationService } from './categorization/UnifiedCategorizationService';
+export { TensorFlowMethod } from './categorization/TensorFlowMethod';
+export { MLEnhancedMethod } from './categorization/MLEnhancedMethod';
+export { RuleBasedMethod } from './categorization/RuleBasedMethod';
+export { TransactionCategorization } from './categorization/TransactionCategorization';
+export { enhancedMLOrchestrator } from './machine-learning/EnhancedMLOrchestrator';
+export { mlCategorizationService } from './machine-learning/MLCategorizationService';
+export { MLIntegrationDashboard } from './machine-learning/MLIntegrationDashboard';
+export { mlNaturalLanguageService } from './machine-learning/MLNaturalLanguageService';
+export { mlPredictiveAnalyticsService } from './machine-learning/MLPredictiveAnalyticsService'; 

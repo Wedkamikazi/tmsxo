4bee0ed38ec90beea228930c8d3fb3c971bc5a7c
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 20:10:08 +0300

    Auto-commit: 1 modified file

--- src/data/integrity/SystemIntegrityService.ts
+++ src/data/integrity/SystemIntegrityService.ts
@@ -1740,7 +1740,7 @@ class SystemIntegrityService {
     const [categorization, predictiveAnalytics, naturalLanguage] = await Promise.allSettled([
               checkMLService('unifiedCategorizationService', (await import('../../analytics/categorization/UnifiedCategorizationService')).unifiedCategorizationService),
       checkMLService('mlPredictiveAnalyticsService', (await import('../../analytics/machine-learning/MLPredictiveAnalyticsService')).mlPredictiveAnalyticsService),
-      checkMLService('mlNaturalLanguageService', (await import('./mlNaturalLanguageService')).mlNaturalLanguageService)
+      checkMLService('mlNaturalLanguageService', (await import('../../analytics/machine-learning/MLNaturalLanguageService')).mlNaturalLanguageService)
     ]);
 
     return {

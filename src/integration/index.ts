3350fe4a60d560a1e1b571c51d71472035159723
Wedkamikazi <wedkamikazi@github.com>
2025-06-17 00:57:08 +0300

    Auto-commit: 1 added file

---
+++ src/integration/index.ts
@@ -0,0 +1,5 @@
+// Integration module exports
+export { localOllamaIntegration } from './ai/LocalOllamaIntegration';
+export { OllamaChat } from './ai/OllamaChat';
+export { OllamaControlWidget } from './ai/OllamaControlWidget';
+export { QwenIntegrationStatus } from './ai/QwenIntegrationStatus'; 

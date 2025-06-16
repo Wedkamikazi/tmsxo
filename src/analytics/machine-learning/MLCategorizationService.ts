4be9be14aa346ed98b8c67c99a1034e66d7b158c
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 20:27:34 +0300

    Auto-commit: src/analytics/machine-learning/MLCategorizationService.ts at 6/16/2025, 8:27:34 PM

--- src/analytics/machine-learning/MLCategorizationService.ts
+++ src/analytics/machine-learning/MLCategorizationService.ts
@@ -1,6 +1,6 @@
 import { isDebugMode as checkDebugMode } from '../../shared/utils/debugging/DebugMode';
 import * as tf from '@tensorflow/tfjs';
-import { Transaction, MLCategorizationResult } from '../../../shared/types';
+import { Transaction, MLCategorizationResult } from '../../shared/types';
 import { categorizationService } from '../categorization/CategorizationService';
 import { localStorageManager } from '../../data/storage/LocalStorageManager';
 import { performanceManager } from '../../core/performance/PerformanceManager';

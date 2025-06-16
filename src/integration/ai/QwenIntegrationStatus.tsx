277c6351a1d25ec7c75bdadbc8eace89fb2be064
Wedkamikazi <wedkamikazi@github.com>
2025-06-16 20:00:08 +0300

    Auto-commit: 1 modified file

--- src/integration/ai/QwenIntegrationStatus.tsx
+++ src/integration/ai/QwenIntegrationStatus.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { unifiedCategorizationService } from '../analytics/categorization/UnifiedCategorizationService';
+import { unifiedCategorizationService } from '../../analytics/categorization/UnifiedCategorizationService';
 import { TensorFlowMethod } from '../services/categorization/tensorFlowMethod';
 import './QwenIntegrationStatus.css';
 
